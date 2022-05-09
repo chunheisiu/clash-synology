8dec69ae1fa93238cd34b9c59fd3d703abca1bc7
412999826 <68527612+412999826@users.noreply.github.com>
2022-05-10 00:26:28 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ latest_release_tag=`get_latest_release Dreamacro/clash`
 [ $arch == 'armv7' ] && board_id='armv7';
 [ $arch == 'armv5' ] && board_id='armv5';
 
-wget https://github.com/Dreamacro/clash/releases/download/$latest_release_tag/clash-linux-$board_id-$latest_release_tag.gz
+wget -q https://github.com/Dreamacro/clash/releases/download/$latest_release_tag/clash-linux-$board_id-$latest_release_tag.gz
 gzip -d clash-linux-$board_id-$latest_release_tag.gz
 chmod +x clash-linux-$board_id-$latest_release_tag
 mv clash-linux-$board_id-$latest_release_tag /usr/bin/clash

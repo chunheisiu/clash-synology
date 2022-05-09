b7432537b10dc18178b9e27068efa51ce75b5358
412999826 <68527612+412999826@users.noreply.github.com>
2022-05-09 23:24:17 +0800

    Update autoupdate.sh

--- autoupdate.sh
+++ autoupdate.sh
@@ -9,7 +9,7 @@ get_latest_release() {
 set -e $1
 
 latest_release_tag=`get_latest_release Dreamacro/clash`
-ver=`/usr/bin/clash -v`
+ver=`/opt/bin/clash -v`
 arch=`uname -m`
 [ $arch == 'x86_64' ] && board_id='amd64';
 [ $arch == 'aarch64' ] && board_id='armv8';
